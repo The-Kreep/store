368e80ea667c94e0df9d7b0d5eae4d7bf9cbd774
Grim's Desktop <paulperson.pro@gmail.com>
2022-08-23 17:02:35 +0200

    removed airtable

--- airtableConfig.js
+++ airtableConfig.js
@@ -1,7 +1,7 @@
-import Airtable from 'airtable'
+// import Airtable from 'airtable'
 
-const base = new Airtable({ apiKey: import.meta.env.AIRTABLE_API_KEY }).base(
-  import.meta.env.AIRTABLE_BASE_ID
-)
+// const base = new Airtable({ apiKey: import.meta.env.AIRTABLE_API_KEY }).base(
+//   import.meta.env.AIRTABLE_BASE_ID
+// )
 
-export { base }
+// export { base }
